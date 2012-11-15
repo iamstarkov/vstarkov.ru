ccc11139aa17de36ceb4722db1177eabb39eebbf
Vladimir Starkov <matmuchrapna@gmail.com>
2012-11-15 17:02:28 +0600

    testing postcommit hook

--- publish.sh
+++ publish.sh
@@ -2,15 +2,22 @@
 git branch -D master
 git checkout -b master
 git filter-branch --subdirectory-filter _site/ -f
-mv _site/* ./ && rm _site -r
-git add . && git commit -m 'update site'
+mv _site/* ./
+rm _site -r
+
+#adding changings
+git add .
+git commit -m 'update site'
 
 
 # sync it to gh-pages
 git branch -D gh-pages
 git checkout -b gh-pages
 touch .nojekyll
-git add . && git commit -m 'update site'
+
+#adding changings
+git add .
+git commit -m 'update site'
 
 # go to source branch
 git checkout source
