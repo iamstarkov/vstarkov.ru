691eef5842c17b3c699706a771c41e2b7ddb259f
Vladimir Starkov <matmuchrapna@gmail.com>
2012-11-15 16:39:49 +0600

    added publish script

--- publish.sh
+++ publish.sh
@@ -1,16 +1,19 @@
-# https://github.com/rson/rson.github.com/blob/source/README.md
-# The only interesting branch in this repo will be the source branch as GitHub 
-# expects the generated website to live in master.
-#
-# git branch -D master
-# git checkout -b master
-# git filter-branch --subdirectory-filter _site/ -f
-# git checkout source
-# git push --all origin
-
+# Extract the generated _site into root of the branch
 git branch -D master
 git checkout -b master
 git filter-branch --subdirectory-filter _site/ -f
-git checkout source
-git push --all origin
+mv _site/* ./ && rm _site -r
+git add . && git commit -m 'update site'
 
+
+# sync it to gh-pages
+git branch -D gh-pages
+git checkout -b gh-pages
+touch .nojekyll
+git add . && git commit -m 'update site'
+
+# go to source branch
+git checkout source
+
+# push it all to the server 
+git push --all -f
